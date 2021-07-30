735a407917e9480dea58c18b1598aed86a02ca31
PolyPup <polypup.farm@gmail.com>
2021-07-30 00:59:31 -0400

    dark mode

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2828,7 +2828,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#fbfff7", backgroundDisabled: "#dde6da", contrast: "#d8f3dc", invertedContrast: "#f1faee", input: "#80ed99", tertiary: "#EFF4F5", text: "#04210a", textDisabled: "#a4a9ab", textSubtle: "#1b4332", borderColor: "#57cc99", card: "#d8f3dc", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#52b788", background: "#081c15", backgroundDisabled: "#E9EAEB", contrast: "#52b788", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#F2DA91", tertiary: "#2f7558", text: "#52b788", textDisabled: "#BDC2C4", textSubtle: "#d8f3dc", borderColor: "#524B63", card: "#1b4332", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#f29559", background: "#081c15", backgroundDisabled: "#E9EAEB", contrast: "#52b788", invertedContrast: "#191326", input: "#40916c", primaryDark: "#F2DA91", tertiary: "#2f7558", text: "#52b788", textDisabled: "#BDC2C4", textSubtle: "#d8f3dc", borderColor: "#524B63", card: "#1b4332", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
